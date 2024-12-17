bf4433d1f45533db5ba77832af0210143dc3579a
twoolums <twoolums@toddwoolums.com>
2024-12-17 12:39:04 -0600

    Creation of Repo

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  url                    = 'https://dahuawiki.com/images/Files/Software/SmartPSS_Lite/SmartPSSLite_V1.003.0000006.0.R.240517.zip'
+  checksum               = 'B4730AF7DD84A383C70A69807E411B056B29A92343D539C4CA38D443EAFBE300'
+  checksumType           = 'sha256'
+  UnzipLocation          = "$env:TMP"
+}
+Install-ChocolateyZipPackage @packageArgs
+
+$packageArgs = @{
+  packageName            = "$env:chocolateyPackageName"
+  FileType               = 'exe'
+  SilentArgs             = '/S'
+  File                   = "$env:TMP\SmartPSSLite_V1.003.0000006.0.R.240517"
+}
+Install-ChocolateyInstallPackage @packageArgs
